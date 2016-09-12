e0540a793f668ba8e9d6206db5acc3e1ccbbbfcb
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-09-13 00:55:55 +0200

    Update info

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-Python project template repository
-==================================
+The equations database
+======================
+This is the backend and API server for one of my dream projects: a highly
+semantic, and computer validated equations database. I inted to use SymPy to
+ensure that all equation derivations in the database are mathematically correct.
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+# Copyright (c) 2016, Herman Bergwerf. All rights reserved.
+# Use of this source code is governed by an AGPL-3.0-style license
+# that can be found in the LICENSE file.
 
 from setuptools import setup, find_packages
 
@@ -9,13 +11,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='template',
+    name='equations_backend',
     version='0.0.1',
-    description='Herman Bergwerfs Python package template',
+    description='Backend for the Equations database',
     long_description=readme,
     author='Herman Bergwerf',
     author_email='hermanbergwerf@gmail.com',
-    url='https://github.com/hermanbergwerf/python-template',
+    url='https://github.com/equations/backend',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
